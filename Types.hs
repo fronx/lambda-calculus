fa1542fc422e09d3481beb5da78b6042d616d9e7
Fronx <fronx@soundcloud.com>
2013-12-31 01:30:37 +0100

    split into multiple files

---
+++ Types.hs
@@ -0,0 +1,20 @@
+module Types where
+
+type VarName  = String
+type TypeName = String
+
+data Error = Error String
+
+data Type = Type TypeName
+          | Type :-> Type
+          deriving (Show, Eq)
+
+data Param = Param VarName Type
+           deriving (Show, Eq)
+
+data Term = Var VarName
+          | Λ Param Term
+          | Apply Term Term
+          deriving (Show, Eq)
+
+type Context = [ (Term, Type) ]
---
+++ Typing.hs
@@ -0,0 +1,77 @@
+module Typing where
+
+import Types
+import Ether
+
+failLookupType :: Term -> String
+failLookupType term = "Type of term " ++ (show term) ++ " unknown."
+
+failDoTypeVar :: Param -> String
+failDoTypeVar param = "Variable names can't be overloaded: " ++ (show param)
+
+failDoTypeApplyT1Fn :: Term -> Type -> String
+failDoTypeApplyT1Fn term typ =
+  "Application not possible. Term " ++ (show term) ++
+  " was expected to be a function, but its type is " ++ (show typ) ++ "."
+
+failDoTypeApplyBadArg :: Term -> Type -> Type -> String
+failDoTypeApplyBadArg term texpected tactual =
+  "Incompatible argument: " ++ (show term) ++
+  " Expected: " ++ (show texpected) ++
+  " Received: " ++ (show tactual)
+
+lookupType :: Term -> Context -> Either String Type
+lookupType term context =
+  maybeToEither (lookup term context) (failLookupType term)
+
+-- force!
+lookupType' :: Term -> Context -> Type
+lookupType' = (.) forceRight . lookupType
+
+typeKnown :: Term -> Context -> Bool
+typeKnown = (.) eitherToBool . lookupType
+
+argType :: Type -> Type
+argType (first :-> rest) = first
+argType t = t
+
+restType :: Type -> Maybe Type
+restType (param :-> rest) = Just rest
+restType _ = Nothing
+
+doType :: Context -> Term -> Either String Context
+doType context (Var vname) =
+  case lookupType (Var vname) context of
+    Left msg    -> Left msg
+    Right _type -> Right $ context
+doType context (Λ (Param pname ptype) body) =
+  if typeKnown (Var pname) context
+    then
+      Left $ failDoTypeVar (Param pname ptype)
+    else
+      let context' = doType ((Var pname, ptype) : context) body
+          lambdaTerm = (Λ (Param pname ptype) body)
+      in case context' of
+           Left msg       -> Left msg
+           Right _context ->
+             Right $ (lambdaTerm, ptype :-> resulttype) : _context
+             where resulttype = lookupType' body _context
+doType context (Apply term1 term2) =
+  ifRight2
+    (doType context term1)
+    (doType context term2)
+    (\conT1 conT2 ->
+      let typeT1 = lookupType' term1 conT1
+          typeT2 = lookupType' term2 conT2
+          argtypeT1 = argType typeT1
+      in if argtypeT1 /= typeT2
+           then
+             Left $ failDoTypeApplyBadArg term2 argtypeT1 typeT2
+           else
+             case restType typeT1 of
+               Nothing -> Left $ failDoTypeApplyT1Fn term1 typeT1
+               Just restTypeT1 -> Right $
+                   ((Apply term1 term2), restTypeT1)
+                 : (term1, typeT1)
+                 : (term2, typeT2)
+                 : conT2)
